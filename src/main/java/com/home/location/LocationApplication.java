d4c15eea0f129e68485afc82d152fd4318c5fc0f
alabenkhlifa <ala.benkhlifa@esprit.tn>
2019-07-26 21:23:36 +0100

    some fixes

--- src/main/java/com/home/location/LocationApplication.java
+++ src/main/java/com/home/location/LocationApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LocationApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(LocationApplication.class, args);
 	}
+
 }

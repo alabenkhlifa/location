6433aaaf32c27396278dd6e48e651223c65f02ad
alabenkhlifa <ala.benkhlifa@esprit.tn>
2019-07-26 21:06:09 +0100

    Merge branch 'master' of C:\Users\ACK\Documents\spring projects\location with conflicts.

--- src/main/java/com/home/location/entities/Paiement.java
+++ src/main/java/com/home/location/entities/Paiement.java
@@ -1,8 +1,35 @@
 package com.home.location.entities;
 
-public class Paiement {
-    //wl paiement fiha date du payement montant et type
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+// Generated Getters and Setters and toString Method
+@Data
+// Generates a default constructor (with no arguments)
+@NoArgsConstructor
+// Generates a constructor with all args
+@AllArgsConstructor
+public class Paiement implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private  java.sql.Date datePaiement;
-    private int montant;
+    @Column(name = "paiementDate")
+    // Date Type : to save the time along side with the date we use TIMESTAMP
+    // TemporalType.DATE => this saves in the DB the date without any time: (2012-02-23 00:00:00)
+    // TemporalType.TIME => this saves in the DB the time without any date: (10:00:00)
+    // TemporalType.TIMESTAMP => this saves in the DB the time and the date: (2012-02-23 10:00:00)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date datePaiement;
+    // = 0L => default value for the attribute, PS : 0L the L stands for Long , because it's of type Long.
+    private Long montant = 0L;
+
+    // TODO : add the relationship (JPA Annotation) and change its type to Loctaion (after creating the entity)
+    private String location;
 }
